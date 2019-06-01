5387c6daa4238a1d49604d9c0c9d7baa5aaafb7b
Nerea <nereaparicio@gmail.com>
2019-06-01 11:23:43 +0200

    Employee model done

---
+++ models/employee.model.js
@@ -0,0 +1,40 @@
+const mongoose = require ('mongoose');
+
+const departments = [ "admin", "rrhh", "support", "contracts", "proyects"]
+
+const employeeSchema = new mongoose.Schema({
+
+    name:{
+      type: String,
+      required: [true, 'The salary is required'],
+      trim: true,
+      },
+
+    salary:{
+      type: Number,
+      required: [true, 'The salary is required'],
+      unique: true,
+      trim: true,
+      },
+
+    department:{
+      type: String,
+      enum: departments,
+      require: [true, 'You must select a department'],
+      }
+
+  },  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      }
+    }
+  });
+  
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
